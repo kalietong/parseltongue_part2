61b93c3c153f8089d8de78ab4bd9fc843751c73a
Kalie Tong <ktong@e1z2r3p44.42.us.org>
2019-09-28 15:42:08 -0700

    hi

---
+++ OO_daenerys.py
@@ -0,0 +1,10 @@
+
+words = str(input(""))
+x= input("DHTFHNUQARFMQMKGSPRLRSKBCMD")
+y= input("Dany")
+
+if words = x:
+    print("Welcome, Daenerys")
+elif words = y:
+    print("Dany who?"
+            )
